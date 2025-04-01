c0c9df413c114641c5feb63e3489e58b684a8dbf
sunil sharma <gitbysuniel@gmail.com>
2025-04-01 15:03:59 +0900

    add prevous month client in homecard

--- server/routes/newRoutes/backupDataRoute.js
+++ server/routes/newRoutes/backupDataRoute.js
@@ -1,15 +1,13 @@
-const express = require ('express');
+// routes/backupRoutes.js
+const express = require('express');
 const router = express.Router();
-const controller = require ('../../controllers/backupDataController');
+const controller = require('../../controllers/backupDataController');
 
-
-router.get('/backup', controller.createBackup);
+router.get('/backup-data', controller.handleManualBackup);
+router.get('/latest-backup', controller.getLatestBackup);
 router.post('/restore', controller.restoreBackup);
-router.get('/schedule', controller.scheduleBackup);
-
-
-
-
-
+router.post('/schedule', controller.scheduleBackup);
+router.post('/stop-schedule', controller.stopScheduledBackup);
+router.get('/schedule-status', controller.getScheduleStatus);
 
 module.exports = router;
